bda13aa6b0c6b4692f9e9ff72244a3cbc1fa2718
wsapi <support@wsapi.chat>
2025-08-21 15:40:45 -0300

    Refactor MediaClient methods to use mediaId instead of MediaDownloadRequest and remove unused MediaDownloadRequest class

--- src/WSApi.Client/ApiClient/MediaClient.cs
+++ src/WSApi.Client/ApiClient/MediaClient.cs
@@ -1,29 +1,27 @@
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using WSApi.Client.Models.Requests.Media;
 
 namespace WSApi.Client.ApiClient;
 
 public class MediaClient(HttpClient httpClient) : IMediaClient
 {
-    public async Task<byte[]> DownloadAsync(MediaDownloadRequest mediaDownloadRequest, CancellationToken cancellationToken = default)
+    public async Task<byte[]> DownloadAsync(string mediaId, CancellationToken cancellationToken = default)
     {
-        var response = await httpClient.PostAsJsonAsync("/media/download", mediaDownloadRequest, cancellationToken: cancellationToken);
+        var response = await httpClient.GetAsync($"/media/download?id={mediaId}", cancellationToken: cancellationToken);
         return await response.EnsureSuccessOrThrowByteArrayAsync();
     }
 
-    public async Task<ApiResponse<byte[]>> TryDownloadAync(MediaDownloadRequest mediaDownloadRequest, CancellationToken cancellationToken = default)
+    public async Task<ApiResponse<byte[]>> TryDownloadAync(string mediaId, CancellationToken cancellationToken = default)
     {
-        var response = await httpClient.PostAsJsonAsync("/media/download", mediaDownloadRequest, cancellationToken: cancellationToken);
+        var response = await httpClient.GetAsync($"/media/download?id={mediaId}", cancellationToken: cancellationToken);
         return await response.ReadAsApiResponseByteArrayAsync();
     }
 }
 
 public interface IMediaClient
 {
-    Task<byte[]> DownloadAsync(MediaDownloadRequest mediaDownloadRequest, CancellationToken cancellationToken = default);
-    
-    Task<ApiResponse<byte[]>> TryDownloadAync(MediaDownloadRequest mediaDownloadRequest, CancellationToken cancellationToken = default);
+    Task<byte[]> DownloadAsync(string mediaId, CancellationToken cancellationToken = default);
+
+    Task<ApiResponse<byte[]>> TryDownloadAync(string mediaId, CancellationToken cancellationToken = default);
 }
